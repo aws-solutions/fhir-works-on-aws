8e623ddb76efd9bda11e4d38c914a5544c8387c1
sevu1 <112517151+sevu1@users.noreply.github.com>
2023-02-01 12:37:29 -0600

    Revert "feat: rbac migration (#14)"
    This reverts commit d383d5347d87bcb7386255847fd8556f9d436bc6.

--- fwoa-core/authz-rbac/src/index.ts
+++
@@ -1,6 +0,0 @@
-/*
- *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *  SPDX-License-Identifier: Apache-2.0
- */
-export * from './RBACConfig';
-export * from './RBACHandler';
--- fwoa-core/authz-smart/src/loggerBuilder.ts
+++
@@ -1,9 +0,0 @@
-import { makeLogger } from '@aws/fhir-works-on-aws-interface';
-
-const componentLogger = makeLogger({
-  component: 'auth-smart'
-});
-
-export default function getComponentLogger(): any {
-  return componentLogger;
-}
