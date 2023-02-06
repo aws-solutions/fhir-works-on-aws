9cb95fcfc0fa01ea47f8f119faa362bf8ea65839
nisankep <nisankep@amazon.com>
2023-02-06 15:32:47 -0500

    test: reverting to ignore quotations which was added by mistake

--- fwoa-core/persistence-ddb/src/constants.ts
+++
@@ -1,8 +0,0 @@
-/*
- *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *  SPDX-License-Identifier: Apache-2.0
- */
-export const SEPARATOR: string = '_';
-export const DDB_HASH_KEY_SEPARATOR: string = '|';
-
-export const MAX_BATCH_WRITE_ITEMS = 25;
