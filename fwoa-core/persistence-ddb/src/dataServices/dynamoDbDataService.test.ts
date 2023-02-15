af04cf5b29a05e1c75d909ed0794895d804022d3
Yanyu <zheyanyu@amazon.com>
2023-02-15 16:33:49 -0500

    feat: remove allowed resource type check from group export

--- fwoa-core/persistence-ddb/src/dataServices/dynamoDbDataService.test.ts
+++ fwoa-core/persistence-ddb/src/dataServices/dynamoDbDataService.test.ts
@@ -564,7 +564,6 @@ describe('updateCreateSupported flag', () => {
 
 describe('initiateExport', () => {
   const initiateExportRequest: InitiateExportRequest = {
-    allowedResourceTypes: ['Patient', 'DocumentReference'],
     requesterUserId: 'userId-1',
     exportType: 'system',
     transactionTime: '2020-09-01T12:00:00Z',
@@ -575,7 +574,6 @@ describe('initiateExport', () => {
   };
 
   const initiateExportRequestWithMultiTenancy: InitiateExportRequest = {
-    allowedResourceTypes: ['Patient', 'DocumentReference'],
     requesterUserId: 'userId-1',
     exportType: 'system',
     transactionTime: '2020-09-01T12:00:00Z',
@@ -631,34 +629,6 @@ describe('initiateExport', () => {
     expect(jobIdWithTenant).toBeDefined();
     expect(ddbPutSpy).toHaveBeenLastCalledWith(
       expect.objectContaining({ Item: expect.objectContaining({ type: { S: 'Patient' } }) })
-    );
-  });
-
-  test('Export request is rejected if user request type they do not have permission for', async () => {
-    // BUILD
-    // Return an export request that is in-progress
-    AWSMock.mock('DynamoDB', 'query', (params: QueryInput, callback: Function) => {
-      if (isEqual(params, DynamoDbParamBuilder.buildQueryExportRequestJobStatus('in-progress'))) {
-        callback(null, {
-          Items: [DynamoDBConverter.marshall({ jobOwnerId: 'userId-2', jobStatus: 'in-progress' })]
-        });
-      }
-      callback(null, {});
-    });
-
-    const ddbPutSpy = jest.fn();
-    AWSMock.mock('DynamoDB', 'putItem', (params: QueryInput, callback: Function) => {
-      ddbPutSpy(params);
-      // Successfully update export-request table with request
-      callback(null, {});
-    });
-
-    const dynamoDbDataService = new DynamoDbDataService(new AWS.DynamoDB());
-    // OPERATE
-    await expect(
-      dynamoDbDataService.initiateExport({ ...initiateExportRequest, type: 'Patient,Group' })
-    ).rejects.toMatchObject(
-      new UnauthorizedError('User does not have permission for requested resource type.')
     );
   });
 
