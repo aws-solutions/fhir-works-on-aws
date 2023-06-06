a1ab13f2e77a447a0d5b3925745fda8acff56688
ssvegaraju <ssvegaraju@yahoo.co.in>
2023-06-06 10:58:20 -0700

    fix: binary tenant prefix

--- fwoa-tools/src/migrationExport.ts
+++ fwoa-tools/src/migrationExport.ts
@@ -114,11 +114,11 @@ if (!dryRun) {
       if (response.exportResponse.output.length === 0) {
         return;
       }
-      let tenantSuffix = '';
+      let tenantPrefix = '';
       if (process.env.MIGRATION_TENANT_ID) {
-        tenantSuffix = `${process.env.MIGRATION_TENANT_ID}/`;
+        tenantPrefix = `${process.env.MIGRATION_TENANT_ID}/`;
       }
-      const names = await sortExportIntoFolders(bucketName, `${tenantSuffix}${response.jobId}/`);
+      const names = await sortExportIntoFolders(bucketName, `${tenantPrefix}${response.jobId}/`);
       output = {
         jobId: output.jobId,
         folderNames: names.folderNames,
