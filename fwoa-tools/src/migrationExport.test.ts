fdce45b0342cf3d41b304915d1f4ec842bb48c1f
Tim Nguyen <thingut@amazon.com>
2023-06-30 15:49:47 -0400

    test: added migrationExport.test.ts.

---
+++ fwoa-tools/src/migrationExport.test.ts
@@ -0,0 +1,79 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+jest.mock('./migrationUtils', () => ({ checkConfiguration: () => {} }));
+jest.mock('./exportHelper', () => ({
+    startExportJob: () => {return {jobId: 'fakeJobId1', jobRunId: 'fakeJobRunId1'}},
+    getExportStatus: () => {},
+    getExportStateFile: () => {return {
+        jobId: 'fakeJobId-1',
+        file_names: ['file1', 'file2']
+    }}
+}));
+
+import {buildRunScriptParams, parseCmdOptions, runScript} from "./migrationExport";
+
+describe('migrationExport', () => {
+    describe('parseCmdOptions', () => {
+        test('smart, dryrun, and since enabled', () => {
+            process.argv = [
+                '/usr/local/bin/ts-node',
+                'migrationExport.ts',
+                '-s',
+                '-d',
+                '-t',
+                '1688144202'
+            ];
+
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const argv: any = parseCmdOptions();
+            expect(argv.dryRun).toEqual(true);
+            expect(argv.smart).toEqual(true);
+            expect(argv.since).toEqual(1688144202);
+        });
+        test('smart, dryrun, and since not enabled', () => {
+            process.argv = [
+                '/usr/local/bin/ts-node',
+                'migrationExport.ts'
+            ];
+
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const argv: any = parseCmdOptions();
+            expect(argv.dryRun).toEqual(false);
+            expect(argv.smart).toEqual(false);
+            expect(argv.since).toBeNull();
+        });
+    })
+
+    test ('buildRunScriptParams', () => {
+        process.argv = [
+            '/usr/local/bin/ts-node',
+            'migrationExport.ts'
+        ];
+        const {smartClient, dryRun, since, snapshotExists, snapshotLocation} = buildRunScriptParams();
+        expect(dryRun).toEqual(false);
+        expect(smartClient).toEqual(false);
+        expect(snapshotExists).toEqual(false);
+        expect(snapshotLocation).toBeNull();
+        expect(since).toBeNull();
+    })
+
+    test('runScript', async() => {
+        await expect(runScript(false, false, '1800-01-01T00:00:00.000Z', false, "" )).resolves.not.toThrowError();
+    })
+    test('runScript - tenantId', async() => {
+        process.env.MIGRATION_TENANT_ID = "tenant1";
+        await expect(runScript(false, false, '1800-01-01T00:00:00.000Z', false, "" )).resolves.not.toThrowError();
+    })
+    test('runScript - invalidSinceDate', async() => {
+            process.argv = [
+                '/usr/local/bin/ts-node',
+                'migrationExport.ts',
+                '-t',
+                'abc'
+            ];
+        await expect(runScript(false, false, 'incorrectSinceDate', false, "" ))
+            .rejects.toThrowError('Provided since timestamp not in correct format (ISO 8601)');
+    })
+})
