704b4e0ec667ac271faa056ea045657340545d81
Robert Smayda <smayda44@gmail.com>
2023-07-07 16:31:21 -0400

    fix: Use const instead of var

--- solutions/deployment/scripts/build_lambda.js
+++ solutions/deployment/scripts/build_lambda.js
@@ -1,17 +1,17 @@
-var fs = require('fs');
-var fse = require('fs-extra');
-var path = require('path');
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
 
 // expected usage: `node build_lambda.js <path> <path> <pathToFile> <fileName>`
 // for use with NodeJsFunction command hooks to add files to Lambda functions,
-// so <path> <path> will usually be the inputDir and outputDir variables, respectively
-var inputDir = process.argv[2];
-var outputDir = process.argv[3];
-var fileToMove = process.argv[4];
-var isDirectory = process.argv.length > 5 ? true : false;
+// so <path> <path> will usually be the inputDir and outputDir constiables, respectively
+const inputDir = process.argv[2];
+const outputDir = process.argv[3];
+const fileToMove = process.argv[4];
+const isDirectory = process.argv.length > 5 ? true : false;
 
 function ensureDirectoryExistence(filePath) {
-  var dirname = path.dirname(filePath);
+  const dirname = path.dirname(filePath);
   if (fs.existsSync(dirname)) {
     return true;
   }
